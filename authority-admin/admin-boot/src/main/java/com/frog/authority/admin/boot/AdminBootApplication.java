d96568b09a562466235d14fb25c2028b0f72807d
liuhuan <liuhuan@gigacloudtech.com>
2022-09-15 15:30:57 +0800

    添加admin-boot模块

---
+++ authority-admin/admin-boot/src/main/java/com/frog/authority/admin/boot/AdminBootApplication.java
@@ -0,0 +1,13 @@
+package com.frog.authority.admin.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminBootApplication.class, args);
+    }
+
+}
---
+++ authority-admin/admin-boot/src/test/java/com/frog/authority/admin/boot/AdminBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frog.authority.admin.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
